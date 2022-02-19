1876758df0bd7a7360a058fdb5f083d8880ed441
boy-eff <80541168+boy-eff@users.noreply.github.com>
2022-02-19 10:14:12 +0300

    Update printFiles2(individual).sh

--- printFiles2(individual).sh
+++ printFiles2(individual).sh
@@ -4,12 +4,15 @@
 #fileName = $3
 if [[ $# -ne 3 ]]; 
 then
-echo "This command must include 3 parameters" >&2
-echo "1 - Minimal file size" >&2
-echo "2 - Maximal file size" >&2
-echo "3 - Name of the file" >&2
+{
+echo "This command must include 3 parameters"
+echo "1 - Minimal file size"
+echo "2 - Maximal file size"
+echo "3 - Name of the file"
+} >&2
 else
-find $pwd -type f -size +$1M -size -$2M -printf "%h %f %s\n" >$3
+find $pwd -type f -size +$1M -size -$2M -exec realpath {} \; -printf "%f %s\n" >$3
 echo "Number of files: "
-cat $3 | wc -l
+numberOfFiles=$(cat $3 | wc -l)
+echo $((numberOfFiles/2))
 fi
