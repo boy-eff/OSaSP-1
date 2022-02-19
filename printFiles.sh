94fed78fa52a750708bfa71b24276690a2d365e1
boy-eff <80541168+boy-eff@users.noreply.github.com>
2022-02-19 10:13:43 +0300

    Update printFiles.sh

--- printFiles.sh
+++ printFiles.sh
@@ -4,17 +4,16 @@
 #extension = $3
 if [[ $# -ne 3 ]]; 
 then
-echo "This command must include 3 parameters" >&2
-echo "1 - Name of the file, where result will be stored" >&2
-echo "2 - Catalog path for searching" >&2
-echo "3 - Extension of the files" >&2
+{
+echo "This command must include 3 parameters"
+echo "1 - Name of the file, where result will be stored"
+echo "2 - Catalog path for searching"
+echo "3 - Extension of the files"
+} >&2
 elif ! [[ -d $2 ]]; then
 echo  "$2 is invalid directory" >&2
 else
 echo -n > $1
-for file in $(find $2 -maxdepth 1 -type f -name "*.$3")
-do
-echo "$file" >> $1
-done
+find $2 -maxdepth 1 -type f -name "*.$3" -printf "%f\n" >> $1
 sort -o $1 $1
 fi
