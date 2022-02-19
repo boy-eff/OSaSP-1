d6c1c75012fe557e92ece594dd56073985a9ea3b
boy-eff <80541168+boy-eff@users.noreply.github.com>
2022-02-19 10:10:21 +0300

    Update compileProg.sh

--- compileProg.sh
+++ compileProg.sh
@@ -3,13 +3,12 @@
 #exeFile = $2
 if [[ $# -ne 2 ]]; 
 then
-echo "This command must include 2 parameters" >&2
-echo "1 - Name of the initial file" >&2
-echo "2 - Name of the executable file" >&2
+{
+echo "This command must include 2 parameters"
+echo "1 - Name of the initial file"
+echo "2 - Name of the executable file" 
+} >&2
 elif ! [[ -f $1 ]]; then
 echo "$1 is not a valid file" >&2
-elif gcc $1 -o $2; then
-./$2;
-else
-echo "File compilation error" >&2
+else gcc $1 -o $2 && ./$2;
 fi
